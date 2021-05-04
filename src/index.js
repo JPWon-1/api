937b8284eaf99af73931543164879623266a5d2d
JPWon-1 <62930150+JPWon-1@users.noreply.github.com>
2021-05-04 23:49:47 +0900

    project Init

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 // index.js
 // This is the main entry point of our application
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 4000;
+app.get('/', (req, res) =>
+  res.send(`hello worldasdasd,server running at http://localhost:${port}`)
+);
+app.listen(port, () =>
+  console.log(`server running at http://localhost:${port}`)
+);
