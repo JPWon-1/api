9782aa492d99b064096df06002eabd5b04336e4a
JPWon-1 <62930150+JPWon-1@users.noreply.github.com>
2021-05-05 13:28:14 +0900

    4장. apollo-server-express 모듈 사용 추가

--- src/index.js
+++ src/index.js
@@ -1,11 +1,63 @@
-// index.js
-// This is the main entry point of our application
 const express = require('express');
-const app = express();
+const { ApolloServer, gql } = require('apollo-server-express');
+//.env 파일에 명시된 포트 또는 포트 4000에서 서버를 실행
 const port = process.env.PORT || 4000;
-app.get('/', (req, res) =>
-  res.send(`hello worldasdasd,server running at http://localhost:${port}`)
-);
-app.listen(port, () =>
-  console.log(`server running at http://localhost:${port}`)
+
+//note data
+let notes = [
+  { id: '1', content: 'This is a note', author: 'Adam Scott' },
+  { id: '2', content: 'This is a another note', author: 'Adam Silver' },
+  { id: '3', content: 'oh hey look . another note!', author: 'Adam Green' }
+];
+
+//그래프 QL 스키마 언어로 스키마를 구성
+const typeDefs = gql`
+  type Note {
+    id: ID!
+    content: String!
+    author: String!
+  }
+  type Query {
+    hello: String!
+    notes: [Note!]!
+    note(id: ID!): Note!
+  }
+  type Mutation {
+    newNote(content: String!): Note!
+  }
+`;
+
+//스키마 필드를 위한 리졸버 함수 제공
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+    notes: () => notes,
+    note: (parent, args) => {
+      return notes.find(note => note.id === args.id);
+    }
+  },
+  Mutation: {
+    newNote: (parent, args) => {
+      let noteValue = {
+        id: String(notes.length + 1),
+        content: args.content,
+        author: 'Adam Scott'
+      };
+      notes.push(noteValue);
+      return noteValue;
+    }
+  }
+};
+
+const app = express();
+
+//아폴로 서버 설정
+const server = new ApolloServer({ typeDefs, resolvers });
+//아폴로 그래프QL 미들웨어를 적용하고 경로를 /api로 설정
+server.applyMiddleware({ app, path: '/api' });
+
+app.listen({ port }, () =>
+  console.log(
+    `GraphQL Server running at http://localhost:${port}${server.graphqlPath}`
+  )
 );
