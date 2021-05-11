55b93b07905001d8111368ff61e422f3e4d5d752
JPWon-1 <62930150+JPWon-1@users.noreply.github.com>
2021-05-11 20:40:38 +0900

    feat : jwt에서 사용자 정보 가져오기

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const db = require('./db');
 const models = require('./models');
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
-
+const jwt = require('jsonwebtoken');
 
 //.env 파일에 명시된 포트 또는 포트 4000에서 서버를 실행
 const port = process.env.PORT || 4000;
@@ -18,15 +18,36 @@ const app = express();
 //DB에 연결
 db.connect(DB_HOST);
 
+//JWT 에서 사용자 정보 가져오기
+const getUser = token => {
+	if(token) {
+		try {
+			//토큰에서 얻은 사용자 정보 반환
+			return jwt.verify(token, process.env.JWT_SECRET);
+		} catch (error) {
+			//토큰에 문제가 있으면 에러 던지기
+			throw new Error('Session Invalid');
+		}
+	}
+}
+
 //아폴로 서버 설정
 const server = new ApolloServer({ 
 	typeDefs, 
 	resolvers,
-	context: () => {
+	context: ({ req }) => {
+		//헤더에서 사용자 토큰 가져오기
+		const token = req.headers.authorization;
+		//토큰에서 사용자 얻기
+		const user = getUser(token);
+		//콘솔에 user 로깅
+		console.log(user);
 		//context에 db models 추가
-		return { models };
+		return { models , user };
 	}
 });
+
+
 
 //아폴로 그래프QL 미들웨어를 적용하고 경로를 /api로 설정
 server.applyMiddleware({ app, path: '/api' });
